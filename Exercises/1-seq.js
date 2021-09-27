90268e239e39675ae5e032ee890299406032be3d
Dimdim28 <wwwdimanes1@gmail.com>
2021-09-27 18:35:26 +0300

    2 commit

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,17 @@
 'use strict';
 
-const seq = (f) => (g) => (x) => 0;
 
+const seqTest = (f) => {
+    return (g) => {            // основной способ использования замыкания функций
+        return g*2 + f;
+        
+}
+}
+console.log(seqTest(7)(5));
+
+const seq = (f) => (g) => (typeof g === 'number' ? f(g) : seq((x) => f(g(x)))); // создаем сложную функцию f(g(x)) с передаваемыми параметрами-функциями f и g
+
+let a = seq(x => x + 7)
+   (x => x * 2)(5)    // вместо f  х = х + 7; вместо g х = 2 * х;  вместо х подставили 5
+console.log(a);
 module.exports = { seq };
